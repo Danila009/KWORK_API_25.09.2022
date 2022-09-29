9d38978edc28b79ca9154dcdb619ead217678012
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2022-09-29 21:58:43 +0400

    code fix

--- FilmsApi/utils/Constants.cs
+++ FilmsApi/utils/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string BASE_URL = "http://localhost:5000";
+        public const string BASE_URL = "http://10.0.2.2:5000";
 
         public const string IMAGE_PATCH = "images";
     }
